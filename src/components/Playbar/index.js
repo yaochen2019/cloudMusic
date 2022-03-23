30c2d86dddac4499d3fc1cbb03c0cf367cb54af1
yaochen2019 <1505297669@qq.com>
2022-03-23 22:45:32 +0800

    second commit for playbar

--- src/components/Playbar/index.js
+++ src/components/Playbar/index.js
@@ -1,21 +1,50 @@
 import React from 'react'
-import { PlaybarWarpper } from './style'
+import { PlaybarWarpper, Bottomwarpper } from './style'
 import { Slider } from 'antd'
-
+import { Link } from 'react-router-dom'
+import 'antd/dist/antd.css'
 
 export default function Playbar() {
 
   function onChange(value) {
     console.log('onChange: ', value);
   }
-  
+
   function onAfterChange(value) {
     console.log('onAfterChange: ', value);
   }
-  
+
   return (
-    <PlaybarWarpper>
-       <Slider   onChange={onChange} onAfterChange={onAfterChange}  defaultValue={30} disabled={false} />
-    </PlaybarWarpper>
+    <Bottomwarpper>
+      <Slider onChange={onChange} onAfterChange={onAfterChange} defaultValue={30} disabled={false} />
+      <PlaybarWarpper>
+
+
+        <div className='img'></div>
+
+        <div className='songinfowarpper' >
+          <div className='songinfo' >
+            <Link to="/recommend"><span>歌名</span></Link>
+            <Link to="/recommend"><span>歌手</span></Link>
+            <Link to="/recommend"><i className='iconfont' >&#xe727;</i></Link>
+          </div>
+          <ul className='smallicon' >
+            <li><i className='iconfont' >&#xe761;</i></li>
+            <li><i className='iconfont' >&#xeb9d;</i></li>
+            <li><i className='iconfont' >&#xe8a6;</i></li>
+            <li><i className='iconfont' >&#xe6ad;</i></li>
+          </ul>
+        </div>
+        <ul className='playbutton' >
+          <li><i className='iconfont' >&#xe6a1;</i></li>
+          <li><i className='iconfont' >&#xe603;</i></li>
+          <li><i className='iconfont' >&#xe62b;</i></li>
+          <li><i className='iconfont' >&#xe602;</i></li>
+          <li><i className='iconfont' >&#xe87a;</i></li>
+        </ul>
+
+
+      </PlaybarWarpper>
+    </Bottomwarpper>
   )
 }
--- src/components/Playbar/style.js
+++ src/components/Playbar/style.js
@@ -1,12 +1,77 @@
 import styled from "styled-components";
 
-export const PlaybarWarpper = styled.div`
-display: flex;
-position: fixed;
-height: 15%;
-width: 83%;
 
+
+export const Bottomwarpper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  z-index: 99;
+  left: 17%;
+  right: 0;
+  bottom: 0;
+  height: 80px;
+  background-position: 0 0;
+  background-repeat: repeat;
+  overflow: visible;
+.ant-slider{
+    
+    z-index: 999;
+    position: relative;
+    left: -6px;
+    top: 10px;
+}
+
+`
+export const PlaybarWarpper = styled.div`
+height: 52px;
+position: relative;
+top: 0;
+display: flex;
+flex-direction: row;
 background-color: rgb(77,161,173);
-margin-left: 20%;
-margin-top: 20%;
+font-size: 5px;
+.img{
+    margin-left: 30px;
+   background-color: red;
+    flex-grow: 0.4;
+}
+.songinfowarpper{
+    display: flex;
+    align-items: center;
+    flex-direction: column;  
+    flex-grow: 0.25;
+    .songinfo{
+        margin-left: -15px;
+        flex-grow: 1;
+        
+    }
+    .smallicon{
+        flex-grow: 1;
+        display: flex;
+        flex-direction: row;
+        li{
+           
+            padding-left: 10px;
+        }
+    }
+
+}
+.playbutton{
+    height: 100%;
+    display: flex;
+    font-size: 20px;
+    position: relative;
+    flex-direction: row;
+    align-items: center;
+    flex-grow: 3;
+    margin-left: 270px;
+    li{
+        
+        padding: 5px;  
+        
+    }
+
+}
+
 `
